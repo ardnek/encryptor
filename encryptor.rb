95e333fe9987bcb347f3b3663c0511890f40b925
Kendra <kdodds@gmail.com>
2015-01-11 12:25:11 -0700

    first commit

---
+++ encryptor.rb
@@ -0,0 +1,41 @@
+class Encryptor
+  def cipher
+    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
+      'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
+      'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
+      'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
+      'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
+      'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
+      'y' => 'l', 'z' => 'm'}
+    end
+
+    def encrypt_letter(letter)
+      lowercase_letter = letter.downcase
+      cipher[lowercase_letter]
+    end
+
+    def encrypt(string)
+      # 1. Cut the input string into letters
+      letters = string.split("")
+      # 2. Encrypt those letters one at a time, gathering the results
+      encrypted = letters.collect do |letter|
+        encrypted_letter = encrypt_letter(letter)
+      end
+      # 3. Join the results back together in one string
+      encrypted.join
+    end
+
+    def decrypt_letter(letter)
+      lowercase_letter = letter.downcase
+      cipher[lowercase_letter]
+    end
+
+    def decrypt(string)
+      letters = string.split("")
+      decrypted = letters.collect do |letter|
+        decrypted_letter = decrypt_letter(letter)
+      end
+      decrypted.join
+    end
+
+end
